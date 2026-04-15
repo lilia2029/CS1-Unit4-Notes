a7b785e186bb7074b93a62e42732175adaad9a07
lilia2029 <lili.aytug2029@gbwl.org>
2026-04-15 14:24:54 +0000

    operators

--- script.js
+++ script.js
@@ -19,5 +19,25 @@ let pokemon = "pikachu";
 let luckyNum = 5;
 luckyNum = 13;
 
-//see value sroed in variable
+//see value stored in variable
 console.log(username);
+console.log(pokemon);
+console.log(luckyNum);
+
+//MATH OPERATIONS (with numbers)
+let age = 18;
+let ageInTen = age + 10;
+let ageInDogYears = age / 7;
+console.log(age);
+console.log(ageInTen);
+console.log(ageInDogYears);
+// You can do math with: +, -, *, /
+
+//combine 2 "literal" Strings with CONCATINATION 
+console.log("My Age is " + "age");
+//combine strings with variables
+console.log("My Age is " + age);
+console.log("In ten years I will be " + ageInTen);
+console.log("My age in dog years " + ageInDogYears);
+//using the plus sign with at least 1 "string"
+//means you're combining (concatination) not doing addition
